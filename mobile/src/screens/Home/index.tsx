2499059c5dba4b6b328dcce21be0fc812b8236db
Bruno Albano <brunomalbano@gmail.com>
2022-09-13 00:42:35 -0300

    criando componentes e a página home

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Image, View, FlatList, ScrollView } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.container}>
+          <Image  
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <Heading 
+            title="Encontre seu duo!"
+            subtitle="Selecione o game que deseja jogar..."
+          />
+
+          <FlatList 
+            data={GAMES}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <GameCard 
+                data={item}
+              />
+            )}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.contentList}
+          />
+        </View>      
+    </ScrollView>
+  );
+}
