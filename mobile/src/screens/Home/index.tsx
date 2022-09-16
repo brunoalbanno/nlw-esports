2e7a2c044256843845be127f451e74c3a21e41eb
Bruno Albano <brunomalbano@gmail.com>
2022-09-15 23:19:30 -0300

    conectando backend com mobile e estilizando página de anúncios

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,16 +1,37 @@
-import { Image, View, FlatList, ScrollView } from 'react-native';
+import { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, FlatList, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
+import { Background } from '../../components/Background';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  const navigation = useNavigation();
+  
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { 
+      id, 
+      title, 
+      bannerUrl
+    });
+  }
+
+  useEffect(() => {
+    fetch('http://192.168.3.4:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
   return (
+    <Background>
       <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
           <Image  
             source={logoImg}
             style={styles.logo}
@@ -22,18 +43,20 @@ export function Home() {
           />
 
           <FlatList 
-            data={GAMES}
+            data={games}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
               <GameCard 
                 data={item}
+                onPress={() => handleOpenGame(item)}
               />
             )}
             horizontal
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={styles.contentList}
           />
-        </View>      
-    </ScrollView>
+        </SafeAreaView>      
+      </ScrollView>
+    </Background>
   );
 }
